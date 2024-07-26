bdb58860523177f46068cadd22634f2a9c9f7906
bcrowe306 <bcrowe306@gmail.com>
2024-07-26 09:19:26 -0400

    Testing iout PNG

--- skin.py
+++ skin.py
@@ -1,3 +1,28 @@
-class SkinColor:
-  def draw(self, control):
+from abc import ABC, abstractmethod
+
+class SkinColor(ABC):
+  """
+  Abstract base class for defining skin colors.
+
+  Subclasses of `SkinColor` should implement the `draw` method to define how the control should be drawn with the
+  specific color scheme.
+
+  Attributes:
+    None
+
+  Methods:
+    draw: Abstract method that should be implemented by subclasses to draw the control with the specific color scheme.
+  """
+
+  @abstractmethod
+  def draw(self, control) -> None:
+    """
+    Abstract method that should be implemented by subclasses to draw the control with the specific color scheme.
+
+    Args:
+      control: The control object to be drawn.
+
+    Returns:
+      None
+    """
     pass
